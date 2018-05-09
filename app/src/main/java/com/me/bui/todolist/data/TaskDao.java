7bb44360a2c26b54d24410a15a6470f84f128d8d
mao.buidinh <mao.buidinh@hotmail.com>
2018-05-10 00:19:34 +0700

    - create Dao.

---
+++ app/src/main/java/com/me/bui/todolist/data/TaskDao.java
@@ -0,0 +1,30 @@
+package com.me.bui.todolist.data;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+/**
+ * Created by mao.bui on 5/10/2018.
+ */
+
+@Dao
+public interface TaskDao {
+
+    @Query("SELECT * FROM task OEDER BY priority")
+    List<TaskEntry> loadAllTask();
+
+    @Insert
+    void insertTask(TaskEntry taskEntry);
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void updateTask(TaskEntry taskEntry);
+
+    @Delete
+    void deleteTask(TaskEntry taskEntry);
+}
