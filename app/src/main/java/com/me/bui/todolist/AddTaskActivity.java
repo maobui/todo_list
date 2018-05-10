2bc9ccdf6a913fe7926c7d76a0b372eba22f5f6e
mao.buidinh <mao.buidinh@hotmail.com>
2018-05-10 23:49:59 +0700

    - update task.

--- app/src/main/java/com/me/bui/todolist/AddTaskActivity.java
+++ app/src/main/java/com/me/bui/todolist/AddTaskActivity.java
@@ -55,6 +55,19 @@ public class AddTaskActivity extends AppCompatActivity {
             mButton.setText(R.string.update_button);
             if (mTaskId == DEFAULT_TASK_ID) {
                 // populate the UI
+                mTaskId = intent.getIntExtra(EXTRA_TASK_ID, DEFAULT_TASK_ID);
+                AppExecutors.getInstance().diskIO().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        final TaskEntry task = mDb.taskDao().loadTaskById(mTaskId);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                populateUI(task);
+                            }
+                        });
+                    }
+                });
             }
         }
     }
@@ -87,7 +100,10 @@ public class AddTaskActivity extends AppCompatActivity {
      * @param task the taskEntry to populate the UI
      */
     private void populateUI(TaskEntry task) {
-
+        if (task == null)
+            return;
+        mEditText.setText(task.getDescription());
+        setPriorityInViews(task.getPriority());
     }
 
     /**
@@ -103,7 +119,13 @@ public class AddTaskActivity extends AppCompatActivity {
         AppExecutors.getInstance().diskIO().execute(new Runnable() {
             @Override
             public void run() {
-                mDb.taskDao().insertTask(entry);
+                if (mTaskId == DEFAULT_TASK_ID) {
+                    mDb.taskDao().insertTask(entry);
+                } else {
+                    // Update task.
+                    entry.setId(mTaskId);
+                    mDb.taskDao().updateTask(entry);
+                }
                 finish();
             }
         });
